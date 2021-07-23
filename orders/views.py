d6048e482fceac5e0924466cbf2774e64a6d82c5
dimasickx <dmitry.sorokin.mail@gmail.com>
2021-07-23 15:42:37 +0300

    mvp

--- orders/views.py
+++ orders/views.py
@@ -9,16 +9,16 @@ from rest_framework import status
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def get_orders(request):
-    if request.method == 'GET':
-        orders = Order.objects.all()
-        serializer_orders = OrderSerializer(orders, many=True)
-        return JsonResponse(serializer_orders.data, safe=False)
-    elif request.method == 'POST':
-        order_data = JSONParser().parse(request)
-        order_serialize = OrderSerializer(data=order_data)
-        if order_serialize.is_valid():
-            order_serialize.save()
-            return JsonResponse(order_serialize.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(order_serialize.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', 'POST'])
+# def get_orders(request):
+#     if request.method == 'GET':
+#         orders = Order.objects.all()
+#         serializer_orders = OrderSerializer(orders, many=True)
+#         return JsonResponse(serializer_orders.data, safe=False)
+#     elif request.method == 'POST':
+#         order_data = JSONParser().parse(request)
+#         order_serialize = OrderSerializer(data=order_data)
+#         if order_serialize.is_valid():
+#             order_serialize.save()
+#             return JsonResponse(order_serialize.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(order_serialize.errors, status=status.HTTP_400_BAD_REQUEST)
